4ae49521d1a3237f7e262c01d562ae4a9d8b3c14
sakitha <sakithabaskaran@gmail.com>
2024-11-13 19:29:31 +0100

    fix slideshow

--- js/script.js
+++ js/script.js
@@ -1,23 +1,18 @@
 let slideIndex = 0;  
 let dots = [];  
-let slideTimeout;  
+let slideTimeout;
+let slideLength;
+
+let imgs = ["../images/Gruppebilde.jpg", "../images/Gruppeledere.jpg"];
+let alts = ["Bilde 1", "Bilde 2"];
 
 function showSlides() {
-    let slides = document.getElementsByClassName("mySlides");
-    dots = document.getElementsByClassName("dot");  
-
-    // Skjul alle slides
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-
-    slides[slideIndex - 1].style.display = "block";
+    let image = document.getElementById("slideImg");
+    image.src = imgs[slideIndex];
 
     updateDots();
 
+    slideIndex = (slideIndex + 1) % slideLength;
     slideTimeout = setTimeout(showSlides, 2000);
 }
 
@@ -25,19 +20,28 @@ function updateDots() {
     for (let i = 0; i < dots.length; i++) {
         dots[i].classList.remove("active");
     }
-
-    dots[slideIndex - 1].classList.add("active");
+    dots[slideIndex].classList.add("active");
 }
 
 function currentSlide(n) {
    
     clearTimeout(slideTimeout);
-
     slideIndex = n;
-    showSlides(); 
-
-    // Start ny tidsklokke etter 2 sekunder
-    slideTimeout = setTimeout(showSlides, 2000);
+    showSlides();
 }
 
+function initalizeDots() {
+    let dotsDiv = document.getElementById("dot-container");
+    for (let i = 0; i < slideLength; i++) {
+        let dot = document.createElement("span");
+        dot.setAttribute("class", "dot");
+        dot.setAttribute("alt", alts[i]);
+        dot.setAttribute("onclick", "currentSlide("+i+")");
+        dots.push(dot);
+        dotsDiv.appendChild(dot);
+    }
+}
+
+slideLength = imgs.length;
+initalizeDots();
 showSlides();
